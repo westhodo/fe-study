7a1efb1190de3777c8b1caa4be5cf565a578f696
hodo <westhodo@gmail.com>
2024-01-18 17:28:26 +0900

    reload 404 error update

--- src/route/index.js
+++ src/route/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import index from '../page/index'
 import desktop from '../page/deskTop'
@@ -21,7 +21,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/fe-study/#/'),
+  history: createWebHashHistory('/fe-study/'),
   routes
 });
 
